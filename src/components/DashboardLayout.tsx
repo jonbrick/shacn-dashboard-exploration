aab4a9dd99dbf94276e4355ba8c949c9cbb6056c
Jon Brick <jonbrick09@gmail.com>
2024-09-16 11:31:05 -0400

    Added dark mode - only changes cards

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ModeToggle } from "./ModeToggle";
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
@@ -7,6 +8,9 @@ interface DashboardLayoutProps {
 export function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
     <div className="min-h-screen p-8 bg-background">
+      <div className="flex justify-end mb-4">
+        <ModeToggle />
+      </div>
       <div className="space-y-6">{children}</div>
     </div>
   );

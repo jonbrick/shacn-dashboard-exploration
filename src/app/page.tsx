64737fd28b3bf826d6fadf52c59e58d198ace404
Jon Brick <jonbrick09@gmail.com>
2024-09-16 10:41:45 -0400

    Added Transactions Card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import data from "@/data/data.json";
 import { Data } from "@/data/types";
 import { AccountCard } from "@/components/AccountCard";
+import { TransactionCard } from "@/components/TransactionsCard";
 
 export default function Home() {
   const appData: Data = data;
@@ -9,6 +10,7 @@ export default function Home() {
     <main className="p-8">
       <h1 className="text-2xl font-bold mb-4">Shadcn Dashboard Exploration</h1>
       <AccountCard accounts={appData.accounts} />
+      <TransactionCard transactions={appData.transactions} />
     </main>
   );
 }
---
+++ src/components/TransactionsCard.tsx
@@ -0,0 +1,44 @@
+import { Transaction } from "@/data/types";
+import { DashboardCard } from "./DashboardCard";
+import { DashboardTable } from "./DashboardTable";
+
+interface TransactionCardProps {
+  transactions: Transaction[];
+}
+
+export function TransactionCard({ transactions }: TransactionCardProps) {
+  const columns = [
+    { header: "Transaction ID", accessorKey: "transactionID" },
+    { header: "Ref ID", accessorKey: "refID" },
+    {
+      header: "Amount",
+      accessorKey: "amount",
+      cell: (transaction: Transaction) =>
+        `$${transaction.amount.toLocaleString()}`,
+    },
+    {
+      header: "Value Date",
+      accessorKey: "valueDate",
+      cell: (transaction: Transaction) =>
+        new Date(transaction.valueDate).toLocaleDateString(),
+    },
+    { header: "Type", accessorKey: "type" },
+    {
+      header: "Status",
+      accessorKey: "statusHistory",
+      cell: (transaction: Transaction) =>
+        transaction.statusHistory[transaction.statusHistory.length - 1].status,
+    },
+  ];
+
+  const content = (
+    <DashboardTable
+      data={transactions.slice(0, 10)} // Only pass the first 10 transactions
+      columns={columns}
+      rowsPerPage={10}
+      totalRows={transactions.length} // Pass the total number of transactions
+    />
+  );
+
+  return <DashboardCard title="Recent Transactions" content={content} />;
+}

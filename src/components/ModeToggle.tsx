bcc4b5125a1a30ace085e7e6d4a6d1bf928e98ff
Jon Brick <jonbrick09@gmail.com>
2024-09-16 12:50:04 -0400

    Added monitor for local

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,11 +1,8 @@
-// components/ModeToggle.tsx
-
 "use client";
 
 import * as React from "react";
-import { Moon, Sun } from "lucide-react";
+import { Moon, Sun, Monitor } from "lucide-react";
 import { useTheme } from "next-themes";
-
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -15,14 +12,27 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Sun
+            className={`h-[1.2rem] w-[1.2rem] transition-all ${
+              theme === "light" ? "rotate-0 scale-100" : "rotate-90 scale-0"
+            }`}
+          />
+          <Moon
+            className={`absolute h-[1.2rem] w-[1.2rem] transition-all ${
+              theme === "dark" ? "rotate-0 scale-100" : "rotate-90 scale-0"
+            }`}
+          />
+          <Monitor
+            className={`absolute h-[1.2rem] w-[1.2rem] transition-all ${
+              theme === "system" ? "rotate-0 scale-100" : "rotate-90 scale-0"
+            }`}
+          />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
